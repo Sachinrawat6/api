f219dd6f7f850729ebdff9086531ae0a2b875384
sachinrawat6 <logistics@qurvii.com>
2025-01-29 10:53:21 +0530

    api

---
+++ index.js
@@ -0,0 +1,17 @@
+const Product = [
+    {
+        StyleNumber:14094,
+        Color:"Black",
+        Type:"Jacket",
+    },
+    {
+        StyleNumber:15020,
+        Color:"Turquoise",
+        Type:"Top",
+    },
+    {
+        StyleNumber:19165,
+        Color:"Beige",
+        Type:"Jacket",
+    },
+]
